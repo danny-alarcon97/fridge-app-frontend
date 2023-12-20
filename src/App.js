e7837a9d8024626575e430bd33d6a7914d8a5fd2
daniel <danny.alarcon1997@gmail.com>
2023-12-20 15:56:09 -0500

    progress on file cleanup

--- src/App.js
+++ src/App.js
@@ -7,20 +7,10 @@ import Navigation from "./components/Navigation";
 import "./App.css";
 
 import HomePage from "./pages/HomePage.js";
-import TopicsPage from "./pages/TopicsPage.js";
-import GalleryPage from "./pages/GalleryPage.js";
-// import OrderPage from "./pages/OrderPage.js";
-// import ContactPage from "./pages/ContactPage.js";
-// import StaffPage from "./pages/StaffPage.js"
-
-import MangasPage from "./pages/MangasPage.js";
-
-import EditMangaPageTable from "./pages/EditMangaPageTable.js";
-import AddMangaPageTable from "./pages/AddMangaPageTable.js";
 
 // Define the function that renders the content in Routes, using State.
 function App() {
-  const [manga, setMangaToEdit] = useState({});
+  const [item, setItemToEdit] = useState({});
 
   return (
     <>
@@ -28,7 +18,6 @@ function App() {
         <header>
           <img id="header-img" src="./favicons/favicon-32x32.png" alt="logo" />
           <h1>Daniel Alarcon Huesca</h1>
-          <p>Web Development Portfolio</p>
         </header>
 
         <Navigation />
@@ -37,24 +26,6 @@ function App() {
           <section>
             <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/topics" element={<TopicsPage />} />
-              <Route path="/gallery" element={<GalleryPage />} />
-
-              {/* 
-              <Route path="/order" element={<OrderPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/staff" element={<StaffPage />} /> */}
-
-              <Route
-                path="/mangas"
-                element={<MangasPage setManga={setMangaToEdit} />}
-              />
-
-              <Route path="/add-manga" element={<AddMangaPageTable />} />
-              <Route
-                path="/edit-manga"
-                element={<EditMangaPageTable mangaToEdit={manga} />}
-              />
             </Routes>
           </section>
         </main>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
-import { MdTopic } from "react-icons/md";
-import { RiGalleryFill } from "react-icons/ri";
-// import { IoIosContact } from "react-icons/io";
-// import { TbTruckDelivery } from "react-icons/tb";
-import { FaBookSkull } from "react-icons/fa6";
 
 function Navigation() {
   return (
@@ -15,42 +10,6 @@ function Navigation() {
           <FaHome />
         </i>
         Home
-      </Link>
-
-      <Link to="/topics">
-        <i>
-          <MdTopic />
-        </i>
-        Topics
-      </Link>
-
-      <Link to="/gallery">
-        <i>
-          <RiGalleryFill />
-        </i>
-        Gallery
-      </Link>
-
-      {/* <Link to="/contact">
-        <i>
-          <IoIosContact />
-        </i>
-        Contact
-      </Link>
-
-      <Link to="/order">
-        <i>
-          <TbTruckDelivery />
-        </i>
-        Order
-      </Link> */}
-
-      {/* <Link to="/staff">Staff Page</Link> */}
-      <Link to="/mangas">
-        <i>
-          <FaBookSkull />
-        </i>
-        Manga List
       </Link>
     </nav>
   );
