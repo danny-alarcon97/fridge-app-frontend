ff17187954da92e38e95b058b515fb0ac97446ba
daniel <danny.alarcon1997@gmail.com>
2024-01-26 22:40:06 -0500

    item boxes progress

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,14 +3,18 @@ import Item from "./Items";
 
 function ItemList({ items, onDelete, onEdit }) {
   return (
-    <table className="item-table">
-      <caption>Add and Edit Kitchen Items</caption>
-      <tbody>
+    <section className="section-meals" id="meals">
+      <div className="container center-text">
+        <span className="subheading">Items</span>
+        <h2 className="heading-secondary">Current Compartment Items</h2>
+      </div>
+
+      <div className="container grid grid--3-cols margin-bottom-md">
         {items.map((item, i) => (
           <Item item={item} key={i} onDelete={onDelete} onEdit={onEdit} />
         ))}
-      </tbody>
-    </table>
+      </div>
+    </section>
   );
 }
 
--- src/components/Items.js
+++ src/components/Items.js
@@ -4,31 +4,65 @@ import { FaTrashCan } from "react-icons/fa6";
 
 function Item({ item, onEdit, onDelete }) {
   return (
-    <tr>
-      <td>
-        <i>
-          <FaRegEdit
-            onClick={() => onEdit(item)}
-            title="Editing will change the item"
-          />
-        </i>
-      </td>
-      <td>{item.name}</td>
-      <td>{item.brand}</td>
-      <td>{item.purchaseDate ? item.purchaseDate.slice(0, 10) : "N/A"}</td>
-      <td>{item.expireDate ? item.expireDate.slice(0, 10) : "N/A"}</td>
-      <td>{item.price}</td>
-      <td>{item.compartment}</td>
-      <td>{item.amountOf}</td>
-      <td>
-        <i>
-          <FaTrashCan
-            onClick={() => onDelete(item._id)}
-            title="Clicking this will delete this item from your kitchen compartment"
-          />
-        </i>
-      </td>
-    </tr>
+    <div className="meal">
+      <img
+        src="./img/customers/ben.jpg"
+        className="meal-img"
+        alt="user photo"
+      />
+      <div className="meal-content">
+        <div className="meal-tags">
+          <span className="tag tag--vegan">Vegan</span>
+        </div>
+        <p className="meal-title">{item.name}</p>
+        <ul className="meal-attributes">
+          <li className="meal-attribute">
+            <span>
+              <strong>Brand:</strong> {item.brand}
+            </span>
+          </li>
+          <li className="meal-attribute">
+            <span>
+              <strong>Purchase Date:</strong>{" "}
+              {item.purchaseDate ? item.purchaseDate.slice(0, 10) : "N/A"}
+            </span>
+          </li>
+          <li className="meal-attribute">
+            <span>
+              <strong>Expiration Date:</strong>{" "}
+              {item.expireDate ? item.expireDate.slice(0, 10) : "N/A"}
+            </span>
+          </li>
+          <li className="meal-attribute">
+            <span>
+              <strong>Price:</strong> {item.price}
+            </span>
+          </li>
+          <li className="meal-attribute">
+            <span>
+              <strong>Compartment:</strong> {item.compartment}
+            </span>
+          </li>
+          <li className="meal-attribute">
+            <span>
+              <strong>Amount Of:</strong> {item.amountOf}
+            </span>
+          </li>
+          <li className="meal-attribute meal-attribute-icons">
+            <FaRegEdit
+              onClick={() => onEdit(item)}
+              title="Edit Item"
+              className="meal-icon"
+            />
+            <FaTrashCan
+              onClick={() => onDelete(item._id)}
+              title="Delete Item"
+              className="meal-icon"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
